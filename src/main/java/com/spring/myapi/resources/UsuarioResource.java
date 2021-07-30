ccffab2ac98343ce4b196679468e3a4e5cb45e9e
Gleison Mozart <mozart.gleison@gmail.com>
2021-07-29 23:53:27 -0300

    Criada a lsita para o retorno da requisição dos Usuarios

--- src/main/java/com/spring/myapi/resources/UsuarioResource.java
+++ src/main/java/com/spring/myapi/resources/UsuarioResource.java
@@ -1,11 +1,14 @@
 package com.spring.myapi.resources;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.myapi.domain.Usuario;
@@ -18,6 +21,15 @@ public class UsuarioResource {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Usuario>> findAll(){
+		
+		List<Usuario> listUsuario = usuarioService.findAll();
+		
+		return ResponseEntity.ok(listUsuario);
+	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Usuario> findById(@PathVariable Integer id){
