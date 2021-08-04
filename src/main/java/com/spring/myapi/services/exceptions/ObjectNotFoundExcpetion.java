d2c5b87fd37fb94b31bc540f4f0cb9fce84480a7
Gleison Mozart <mozart.gleison@gmail.com>
2021-08-04 00:50:58 -0300

    Criando as Exceções

---
+++ src/main/java/com/spring/myapi/services/exceptions/ObjectNotFoundExcpetion.java
@@ -0,0 +1,20 @@
+package com.spring.myapi.services.exceptions;
+
+public class ObjectNotFoundExcpetion extends RuntimeException {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundExcpetion(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ObjectNotFoundExcpetion(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
